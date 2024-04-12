733e0b8f5d9fb28be6f5d98c8f0e36b00076c188
Daniel Barden <dbarden@seatgeek.com>
2024-04-12 09:41:16 -0300

    First SDK release

--- Package.swift
+++ Package.swift
@@ -9,21 +9,17 @@ let package = Package(
         .iOS(.v15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RotatingBarcodesSDK",
             targets: ["RotatingBarcodesSDK"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "RotatingBarcodesSDK",
-            url: "https://seatgeek.jfrog.io/artifactory/rotating-barcodes-sdk-ios/v0.0.1/RotatingBarcodesSDK.xcframework.zip",
-            checksum: "b5d8f842aaba06035af86aa7d1070da23745d7034114eaa2ae40313db9bff69f"
+            url: "https://seatgeek.jfrog.io/artifactory/rotating-barcodes-sdk-ios/v1.0.0/RotatingBarcodesSDK.xcframework.zip",
+            checksum: "d9838e8a9f7622e6fb34333ba8e486bb52df9280b968879c7ee155dd21316830"
         )
     ]
 )
